ca1e4a965650525710d67365f620dc97a313360a
Richard Moe <richmoe@hotmail.com>
2021-02-08 07:13:46 -0800

    Restructure files

---
+++ DunGen/Models/TrapType.swift
@@ -0,0 +1,39 @@
+//
+//  TrapType.swift
+//  DunGen
+//
+//  Created by Richard Moe on 2/6/21.
+//  Copyright © 2021 Richard Moe. All rights reserved.
+//
+
+import Foundation
+
+class Trap {
+    
+    
+    var name = ""
+    var description = ""
+    var type: Int = 0
+    
+    let typeDoor = 1
+    let typeChest = 2
+    let typePit = 3
+    
+    
+    var damage = ""
+    
+    
+    
+    func random(cr: Double, trapType: Int = 0) {
+        
+        type = trapType
+
+        
+        
+        
+        
+        
+    }
+    
+    
+}
